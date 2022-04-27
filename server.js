4c19e1883591bf4e33394a2dfd1478e0774730bc
RungrojYad <rungroj.yad@gmail.com>
2022-04-27 19:23:40 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,84 @@
+/*
+In terminal,
+>npm i express cors mysql2
+>npm i -g nodemon
+>nodemon server.js
+
+Testing in Browser,
+localhost:5000/users
+
+git remote add clever git+ssh://git@push-n2-par-clevercloud-customers.services.clever-cloud.com/app_752d4717-3fdf-4ee4-afd1-64558b277c43.git
+git push clever master
+*/
+
+
+var express = require('express')
+var cors = require('cors')
+const mysql = require('mysql2');
+const PORT = process.env.PORT || 5000
+
+const connection = mysql.createConnection({
+  host: 'bcigocqmvixvipxf2tzd-mysql.services.clever-cloud.com',
+  port: '3306',
+  user: 'uiegzb0nnozq3vlg',
+  password: 'WCGWIN9lIzwPz4shyUfD',
+  database: 'bcigocqmvixvipxf2tzd'
+});
+
+var app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/users', function (req, res, next) {
+  connection.query(
+    'SELECT * FROM `users`',
+    function(err, results, fields) {
+      res.json(results);
+    }
+  );
+})
+
+app.get('/users/:id', function (req, res, next) {
+  const id = req.params.id;
+  connection.query(
+    'SELECT * FROM `users` WHERE `id` = ?',
+    [id],
+    function(err, results) {
+      res.json(results);
+    }
+  );
+})
+
+app.post('/users', function (req, res, next) {
+  connection.query(
+    'INSERT INTO `users`(`fname`, `lname`, `username`, `password`, `avatar`) VALUES (?, ?, ?, ?, ?)',
+    [req.body.fname, req.body.lname, req.body.username, req.body.password, req.body.avatar],
+    function(err, results) {
+      res.json(results);
+    }
+  );
+})
+
+app.put('/users', function (req, res, next) {
+  connection.query(
+    'UPDATE `users` SET `fname`= ?, `lname`= ?, `username`= ?, `password`= ?, `avatar`= ? WHERE id = ?',
+    [req.body.fname, req.body.lname, req.body.username, req.body.password, req.body.avatar, req.body.id],
+    function(err, results) {
+      res.json(results);
+    }
+  );
+})
+
+app.delete('/users', function (req, res, next) {
+  connection.query(
+    'DELETE FROM `users` WHERE id = ?',
+    [req.body.id],
+    function(err, results) {
+      res.json(results);
+    }
+  );
+})
+
+app.listen(PORT, function () {
+  console.log('CORS-enabled web server listening on port '+PORT)
+})
